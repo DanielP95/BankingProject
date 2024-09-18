9a51e0a05690a565e18b512c4cccdbfee6b66f3f
Daniel Pimentel <danielpimentel@Daniels-MBP.lan>
2024-09-18 16:58:11 -0700

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,39 @@
+import RightSidebar from '@/components/RightSidebar';
+import HeaderBox from '@/components/ui/HeaderBox';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: 'Daniel',
+    lastName: 'Pimentel',
+    email: 'djpimentel@gmail.com',
+  };
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.44}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1234.7 }, { currentBalance: 500.24 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
